55e7fb3dee3b2d40b2ef5a496f3d29c31cf824c9
Jason Tucker <jtgeek@gmail.com>
2015-11-11 12:20:28 -0500

    a

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,1 +1,35 @@
-﻿internal class A{private static void Main(){for (var i = 0; i++ < 100; System.Console.WriteLine(i%3 < 1 ? "Fizz" : "") + (i%5 < 1 ? "Buzz" : i%3 < 1 ? "" : i + "")));}}
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+public static class A
+{
+    public static void Main(string[] args)
+    {
+        var numbers = new List<int>
+        {
+            90,109,82,53,66,81,82,106,66,81,82,106
+        };
+
+        var letters = numbers.Select(x => (x/2)).ToList();
+
+
+        for (var i = 0; i > -100; i--)
+        {
+            foreach (var letter in letters)
+            {
+                Console.Write((char)((letter + i) + 64));
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+
+    public static int FindModInverse(int input)
+    {
+        return 1;
+    }
+}
